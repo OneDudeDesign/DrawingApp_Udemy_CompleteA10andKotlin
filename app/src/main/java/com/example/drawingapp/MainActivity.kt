66e2009b1328669a6b5d206987df788343107b6b
Christopher Lucier <OneDudeDesign@gmail.com>
2021-01-04 13:55:26 -0800

    Phase01 Permissions Added for read and write storage when user clicks the image buttton, getting an image is to be done later

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.drawingapp
 
+import android.Manifest
 import android.app.Dialog
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
@@ -22,31 +26,40 @@ class MainActivity : AppCompatActivity() {
 
         mImageButtonCurrentPaint = ll_paintColors[1] as ImageButton
         mImageButtonCurrentPaint!!.setImageDrawable(
-            ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
         )
         ib_brush.setOnClickListener {
             showBrushSizeChooserDialogue()
         }
+
+        ib_gallery.setOnClickListener {
+            if (isReadStorageAllowed()) {
+                //run code to get the image from the gallery
+            } else {
+                //we need to get permissions if we do not have them
+                requestStoragePermission()
+            }
+        }
     }
 
-    private fun showBrushSizeChooserDialogue(){
+    private fun showBrushSizeChooserDialogue() {
         val brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialogue_brush_size)
         brushDialog.setTitle("Brush size: ")
         val smallBtn = brushDialog.ib_small_brush
-        smallBtn.setOnClickListener{
+        smallBtn.setOnClickListener {
             drawing_view.setSizeForBrush(10.toFloat())
             brushDialog.dismiss()
         }
 
         val mediumBtn = brushDialog.ib_medium_brush
-        mediumBtn.setOnClickListener{
+        mediumBtn.setOnClickListener {
             drawing_view.setSizeForBrush(20.toFloat())
             brushDialog.dismiss()
         }
 
         val largeBtn = brushDialog.ib_large_brush
-        largeBtn.setOnClickListener{
+        largeBtn.setOnClickListener {
             drawing_view.setSizeForBrush(30.toFloat())
             brushDialog.dismiss()
         }
@@ -54,21 +67,64 @@ class MainActivity : AppCompatActivity() {
         brushDialog.show()
     }
 
-    fun paintClicked(view: View){
-        if (view !== mImageButtonCurrentPaint){
+    fun paintClicked(view: View) {
+        if (view !== mImageButtonCurrentPaint) {
             val imageButton = view as ImageButton
 
             val colorTag = imageButton.tag.toString()
             drawing_view.setColor(colorTag)
             imageButton.setImageDrawable(
-                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+                    ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
             )
             mImageButtonCurrentPaint!!.setImageDrawable(
-                ContextCompat.getDrawable(this,R.drawable.pallet_normal)
+                    ContextCompat.getDrawable(this, R.drawable.pallet_normal)
             )
             mImageButtonCurrentPaint = view
         }
 
 
+    }
+
+    private fun requestStoragePermission() {
+        //first block is to show rationale for our permission request to the user
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE).toString())) {
+            Toast.makeText(this, "Need permission to add a background image",
+                    Toast.LENGTH_SHORT).show()
+        }
+        //now request the permissions
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
+    }
+
+    //not required but shows what is going on
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //check if the permission code was our permission code in the companion object
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            //did we get access?
+            if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted, " +
+                        "now you can read and write the storage files", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Oops you denied the permissions.",
+                        Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    //helper function to determine if we still have permission to read storage
+    private fun isReadStorageAllowed(): Boolean {
+        val result = ContextCompat.checkSelfPermission(this,
+        Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val STORAGE_PERMISSION_CODE = 1
     }
 }
